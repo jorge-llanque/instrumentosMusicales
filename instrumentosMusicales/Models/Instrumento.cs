f70d3b9ac3f37226cb2d3935f406d6e4e2f445e4
jorge <georgellanque@gmail.com>
2021-12-27 01:16:52 -0400

    first commit

---
+++ instrumentosMusicales/Models/Instrumento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace instrumentosMusicales.Models
+{
+    public class Instrumento
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public virtual TipoInstrumento TipoInstrumento { get; set; }
+
+    }
+}
---
+++ instrumentosMusicales/Models/TipoInstrumento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace instrumentosMusicales.Models
+{
+    public class TipoInstrumento
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        public virtual List<Instrumento> Instrumentos { get; set; }
+    }
+}
